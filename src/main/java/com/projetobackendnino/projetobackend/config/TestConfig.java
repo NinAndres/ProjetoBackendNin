e5499569d3fbb83c696d372e3711fbbc2e194d4b
Nicolas Andres Kurek <131825518+NinAndres@users.noreply.github.com>
2024-02-09 16:05:55 -0300

    implements payment one-to-one

--- src/main/java/com/projetobackendnino/projetobackend/config/TestConfig.java
+++ src/main/java/com/projetobackendnino/projetobackend/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.projetobackendnino.projetobackend.entities.Category;
 import com.projetobackendnino.projetobackend.entities.Order;
 import com.projetobackendnino.projetobackend.entities.OrderItem;
+import com.projetobackendnino.projetobackend.entities.Payment;
 import com.projetobackendnino.projetobackend.entities.Product;
 import com.projetobackendnino.projetobackend.entities.User;
 import com.projetobackendnino.projetobackend.entities.enums.OrderStatus;
@@ -79,6 +80,10 @@ public class TestConfig implements CommandLineRunner {
     OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
     orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+    Payment pay1 = new Payment(null, Instant.parse("2022-06-20T21:53:07Z"), o1);
+    o1.setPayment(pay1);
+    orderRepository.save(o1);
 
   }
 }
